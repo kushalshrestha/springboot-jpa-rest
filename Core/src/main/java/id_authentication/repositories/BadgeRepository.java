b7e68c4ddda56a4f2f73fbc719b4c31a7cd7fba6
Shamim Babirye <shamim.babirye@miu.edu>
2023-03-19 17:16:52 +0000

    Merged PR 6: renamed repo

---
+++ Core/src/main/java/id_authentication/repositories/BadgeRepository.java
@@ -0,0 +1,9 @@
+package id_authentication.repositories;
+
+import id_authentication.domain.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BadgeRepository extends JpaRepository<Badge, Long> {
+}
---
+++ Core/src/main/java/id_authentication/repositories/MembershipRepository.java
@@ -0,0 +1,9 @@
+package id_authentication.repositories;
+
+import id_authentication.domain.Membership;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership, Long>{
+}
