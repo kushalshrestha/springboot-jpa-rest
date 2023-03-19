81d1dc28d0de7b0b06553d4e9e6b46a5d087a80b
omarkhris <omar2009kh@gmail.com>
2023-03-19 13:55:38 -0500

    Deleted Unnecessarily Repos

--- Core/src/main/java/id_authentication/repositories/Plan.java
+++
@@ -1,8 +0,0 @@
-package id_authentication.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Plan extends JpaRepository<Plan, Long>{
-}
--- Core/src/main/java/id_authentication/repositories/RolePlanLimit.java
+++
@@ -1,8 +0,0 @@
-package id_authentication.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolePlanLimit extends JpaRepository<RolePlanLimit, Long>{
-}
