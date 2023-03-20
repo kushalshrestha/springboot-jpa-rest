09c5792431169ce024b0487c826b93b7de2ac552
sritipraz <prajapatisriti.sp@gmail.com>
2023-03-19 23:51:59 -0500

    Security Config and Exception Handler

--- Core/src/main/java/id_authentication/controller/MemberController.java
+++ Core/src/main/java/id_authentication/controller/MemberController.java
@@ -1,5 +1,11 @@
 package id_authentication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/member")
 public class MemberController {
 
 
