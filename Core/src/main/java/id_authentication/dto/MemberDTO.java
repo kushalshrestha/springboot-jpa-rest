f57af11cbaf59dcffb0029e51c5a7e3587e762ff
Ferdos Ahmed Sied <ferdos.sied@miu.edu>
2023-03-20 18:08:36 +0000

    Merged PR 16: FEAT: created remaining DTO classes.

--- Core/src/main/java/id_authentication/dto/MemberDTO.java
+++ Core/src/main/java/id_authentication/dto/MemberDTO.java
@@ -1,4 +1,37 @@
 package id_authentication.dto;
+import id_authentication.domain.Badge;
+import id_authentication.domain.Membership;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class MemberDTO {
+    private long id;
+    private String memberNumber;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private List<Membership> memberships;
+    private List<Badge> badges = new ArrayList<Badge>();
+
+    public MemberDTO(List<Membership> memberships, List<Badge> badges) {
+
+    }
+
+    public MemberDTO(long id, String firstName, String lastName, List<Membership> memberships,
+                     List<Badge> badges, String userName, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.memberships = memberships;
+        this.badges = badges;
+    }
+
 }
