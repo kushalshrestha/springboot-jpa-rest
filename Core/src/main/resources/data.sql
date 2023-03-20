6845de268f8b1fad640354651cbfddf9597b45a0
Kushal Shrestha <615640@cs.mum.edu>
2023-03-19 22:59:01 -0500

    dummy data for member

--- Core/src/main/resources/data.sql
+++ Core/src/main/resources/data.sql
@@ -1,3 +1,11 @@
 
 INSERT INTO [dbo].[Role] (name)
 VALUES ('Student'),('Faculty'),('Staff');
+
+
+INSERT INTO [dbo].[Member] (first_name, last_name, password, user_name)
+values
+    ('Kushal','Shrestha', 'test123','kushal.shrestha'),
+    ('Jane', 'Andersen', 'bruce.lester', 'jane.andersen'),
+    ('Peter', 'Adley', 'peter.adley', 'peter.adley');
+
