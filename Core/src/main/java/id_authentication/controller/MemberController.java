cf9aab9df2d5aae60f0c8753fe6629e3004de790
sritipraz <prajapatisriti.sp@gmail.com>
2023-03-20 16:20:08 -0500

    member crud

--- Core/src/main/java/id_authentication/controller/MemberController.java
+++ Core/src/main/java/id_authentication/controller/MemberController.java
@@ -1,13 +1,45 @@
 package id_authentication.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import id_authentication.dto.MemberDTO;
+import id_authentication.dto.collection.MemberDTOs;
+import id_authentication.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 public class MemberController {
 
+    @Autowired
+    MemberService memberService;
+        @GetMapping("/{id}")
+        public ResponseEntity<?> getMember(@PathVariable String id){
+            MemberDTO memberDTO=memberService.getMember(Long.parseLong(id));
+            return new ResponseEntity<MemberDTO>(memberDTO, HttpStatus.OK);
+        }
+
+        @PostMapping("")
+        public ResponseEntity<?> createMember(@RequestBody MemberDTO memberDTO){
+            MemberDTO createdMemberDTO=memberService.createMember(memberDTO);
+            return new ResponseEntity<MemberDTO>(createdMemberDTO, HttpStatus.CREATED);
+        }
+        @PutMapping("/{id}")
+        public ResponseEntity<?> updateMember(@PathVariable String id,@RequestBody MemberDTO memberDTO){
+            MemberDTO updatedMemberDTO=memberService.updateMember(Long.parseLong(id),memberDTO);
+            return new ResponseEntity<MemberDTO>(updatedMemberDTO, HttpStatus.OK);
+        }
+
+        @GetMapping("")
+        public ResponseEntity<?> getAllMembers(){
+            MemberDTOs memberDTOs=memberService.getAllMembers();
+            return new ResponseEntity<MemberDTOs>(memberDTOs, HttpStatus.OK);
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<?> deleteMember(@PathVariable String id){
+            memberService.deleteMember(Long.parseLong(id));
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
 }
--- Core/src/main/java/id_authentication/repositories/MemberRepository.java
+++ Core/src/main/java/id_authentication/repositories/MemberRepository.java
@@ -2,9 +2,23 @@ package id_authentication.repositories;
 
 import id_authentication.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    public void findMemberByMemberNumber(String memberNumber);
+    public Member findMemberByMemberNumber(String memberNumber);
+
+    @Query("select m from Member m  join fetch m.memberships" )
+    public Member findByIdEagerFetchMembership(@Param("memberId") Long memberId);
+
+
+    @Query("select m from Member m  join fetch m.badges" )
+    public Member findByIdEagerFetchBadge(@Param("memberId") Long memberId);
+
+    @Query("select m from Member m  join fetch m.checkInRecords" )
+    public Member findByIdEagerFetchCheckInRecords(@Param("memberId") Long memberId);
 }
