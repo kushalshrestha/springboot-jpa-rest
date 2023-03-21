0356a16cecfd1845a209dab27e953d8208bc8891
Kushal Shrestha <kushal.shrestha@miu.edu>
2023-03-21 02:45:34 +0000

    Merged PR 18: FEAT: CRUD for Membership

--- Core/src/main/java/id_authentication/controller/MemberController.java
+++ Core/src/main/java/id_authentication/controller/MemberController.java
@@ -1,13 +1,27 @@
 package id_authentication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import id_authentication.dto.request.MembershipRequestDto;
+import id_authentication.dto.response.MembershipResponseDto;
+import id_authentication.service.IMembershipService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberController {
 
+    private IMembershipService membershipService;
+    private ModelMapper modelMapper;
 
+    @GetMapping("{memberId}/memberships")
+    public ResponseEntity<?> findMembershipsByMemberId(@PathVariable String memberId){
+        List<MembershipResponseDto> membershipResponseDto = membershipService.findAllByMemberId(memberId);
+        return new ResponseEntity<List<MembershipResponseDto>>(membershipResponseDto, HttpStatus.OK);
+    }
 }
