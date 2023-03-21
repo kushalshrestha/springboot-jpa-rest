8b6b8b95f276bf753875252f0fe35512b4e4a5f4
Kushal Shrestha <615640@cs.mum.edu>
2023-03-21 15:55:55 -0500

    transaction controller setup for read, update and delete

--- Core/src/main/java/id_authentication/controller/TransactionController.java
+++ Core/src/main/java/id_authentication/controller/TransactionController.java
@@ -1,8 +1,53 @@
 package id_authentication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/api/v1/transaction")
+import id_authentication.errorhandler.CustomErrorType;
+import id_authentication.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
 public class TransactionController {
 
+    @GetMapping
+    public ResponseEntity<?> getTransactions() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body("HELLO");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new CustomErrorType("Error retrieving transactions: " + e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTransaction(@PathVariable("id") long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body("GET METHOD - by ID");
+        } catch (Exception e) {
+            CustomErrorType customErrorType = new CustomErrorType("Error retrieving transaction with id " + id + ": " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(customErrorType);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTransaction(@PathVariable long id, @RequestBody String text) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(text);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new CustomErrorType(e.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTransaction(@PathVariable long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body("Successfully Deleted!!");
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new CustomErrorType(e.getMessage()));
+        }
+    }
 }
--- Core/src/main/java/id_authentication/service/TransactionService.java
+++ Core/src/main/java/id_authentication/service/TransactionService.java
@@ -1,4 +1,7 @@
 package id_authentication.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface TransactionService {
 }
