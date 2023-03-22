2032595bd1775cccd39c08cd96d68c25b3c028f2
omarkhris <omar2009kh@gmail.com>
2023-03-22 12:39:47 -0500

    added Authorities for Member and Checker Role

--- Core/src/main/java/id_authentication/config/WebSecurityConfig.java
+++ Core/src/main/java/id_authentication/config/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package id_authentication.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,23 +22,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.inMemoryAuthentication()
                 .passwordEncoder(encoder)
-                .withUser("service")
-                .password(encoder.encode("d9c9a5fb-4fc6-4084-b0bb-66e1aaf6813b"))
-                .roles("Service");
+                .withUser("checker")
+                .password(encoder.encode("admin"))
+                .roles("Checker")
+                .and()
+                .withUser("member")
+                .password(encoder.encode(("user")))
+                .roles("Member");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /*
+        Stateless session enables authentication for every request.
+        */
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/favicon.ico").permitAll()
-                .anyRequest().hasRole("Service")
-//			.and()
-//			.formLogin()
+                .antMatchers(HttpMethod.GET, "/api/v1/plans/{id}").hasAnyRole("Member", "Checker")// Any Member can retrieve the list of plans he/she has
+                .antMatchers(HttpMethod.GET,"/api/v1/memberships/{id}").hasAnyRole("Member", "Checker")// A Member And Checker Can retrieve the List of all membership
+                .antMatchers("/**").hasRole("Checker")// A Checker has the Authority to access each any every endpoint
+                .anyRequest()
+                .authenticated()
                 .and()
-                .csrf()
-                .disable()
                 .httpBasic();
+
     }
 
 }
